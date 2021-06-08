36723d7c643136acab39b0de8dcb2211c158671d
JimChenWYU <JimChenWYU@users.noreply.github.com>
2021-06-08 01:26:33 +0000

    Fix styling

--- src/Console/InstallCommand.php
+++ src/Console/InstallCommand.php
@@ -92,7 +92,7 @@ class InstallCommand extends Command
 
         $dockerCompose = file_get_contents(__DIR__ . '/../../stubs/docker-compose.stub');
 
-        $dockerCompose = str_replace('{{depends}}', empty($depends) ? '' : '        '.$depends, $dockerCompose);
+        $dockerCompose = str_replace('{{depends}}', empty($depends) ? '' : '        ' . $depends, $dockerCompose);
         $dockerCompose = str_replace('{{services}}', $stubs, $dockerCompose);
         $dockerCompose = str_replace('{{volumes}}', $volumes, $dockerCompose);
 
